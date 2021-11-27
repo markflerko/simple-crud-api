9061a880fddd99bb893afdd0adc4da62e93c796e
markflerko <mark.flerko@satelliteinnovations.io>
2021-11-27 22:34:15 +0300

    feat: post, required fields

--- src/controllers/postPerson.js
+++ src/controllers/postPerson.js
@@ -4,8 +4,21 @@ const responseBuilder = require("../utils/responseBuilder");
 
 const postPerson = async (req, res) => {
   await bodyParser(req);
-  const person = createPerson({ data: req.body });
-  responseBuilder({ res, code: 201, body: person });
+  const data = req.body;
+  if (
+    !data.hasOwnProperty("name") ||
+    !data.hasOwnProperty("age") ||
+    !data.hasOwnProperty("hobbies")
+  ) {
+    responseBuilder({
+      res,
+      code: 400,
+      message: `You didn't provide one of required fields, please check name: ${data.name} age: ${data.age} hobbies: ${data.hobbies}\n`,
+    });
+  } else {
+    const person = createPerson({ data });
+    responseBuilder({ res, code: 201, body: person });
+  }
 };
 
 module.exports = postPerson;
