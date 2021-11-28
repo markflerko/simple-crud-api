25e4e48a5a96e533a75e4af0c6996fd24f0132d5
markflerko <mark.flerko@satelliteinnovations.io>
2021-11-28 21:04:09 +0300

    feat: 2 test

--- tests/index.test.js
+++
@@ -1,8 +0,0 @@
-const server = require("../index");
-const request = require("supertest");
-
-test("yolo", async () => {
-  const response = await request(server).get("/person");
-
-  expect(response.status).toBe(200);
-});
